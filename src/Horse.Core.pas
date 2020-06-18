aeca4df08e64c10eb6eb2e3cd72442bc8068cfbb
Danilo Lucas <developer.dlio@gmail.com>
2020-06-18 12:16:00 -0300

    Improvement Router Method
    Enable multiple get and post methods without having to specify path again.

--- src/Horse.Core.pas
+++ src/Horse.Core.pas
@@ -2,83 +2,9 @@ unit Horse.Core;
 
 interface
 
-uses System.SysUtils, Web.HTTPApp, Horse.Router;
+uses System.SysUtils, Web.HTTPApp, Horse.Router, Horse.Core.Route.Intf;
 
 type
-  THorseCore = class;
-
-  IHorseCoreRoute = interface
-    ['{39ABE601-E812-40B4-8857-F14C938B9773}']
-    function Get(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Get(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Get(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Get(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Put(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Put(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Put(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Put(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Patch(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Patch(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Patch(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Patch(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Head(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Head(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Head(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Head(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Post(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Post(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Post(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Post(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Delete(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Delete(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Delete(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Delete(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-  end;
-
-  THorseCoreRoute = class(TInterfacedObject, IHorseCoreRoute)
-  private
-    FPath:string;
-    FHorseCore:THorseCore;
-
-  public
-    constructor Create(APath:string; AHorseCore: THorseCore);
-
-    function Get(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Get(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Get(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Get(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Put(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Put(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Put(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Put(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Patch(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Patch(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Patch(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Patch(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Head(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Head(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Head(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Head(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Post(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Post(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Post(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Post(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-
-    function Delete(ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Delete(AMiddleware, ACallback: THorseCallback):IHorseCoreRoute; overload;
-    function Delete(ACallbacks: array of THorseCallback):IHorseCoreRoute; overload;
-    function Delete(ACallbacks: array of THorseCallback; ACallback: THorseCallback):IHorseCoreRoute; overload;
-  end;
-
   THorseCore = class
   private
     FRoutes: THorseRouterTree;
@@ -134,6 +60,9 @@ type
   end;
 
 implementation
+
+uses
+  Horse.Core.Route;
 
 { THorseCore }
 
@@ -248,7 +177,7 @@ end;
 
 function THorseCore.Route(APath: string): IHorseCoreRoute;
 begin
-  Result:= THorseCoreRoute.Create(APath, Self);
+  Result := THorseCoreRoute.Create(APath, Self);
 end;
 
 class destructor THorseCore.UnInitialize;
@@ -365,158 +294,6 @@ begin
   for LCallback in ACallbacks do
     Put(APath, LCallback);
   Put(APath, ACallback);
-end;
-
-{ THorseCoreRoute }
-
-constructor THorseCoreRoute.Create(APath:string; AHorseCore: THorseCore);
-begin
-  FPath      := APath;
-  FHorseCore := AHorseCore;
-end;
-
-function THorseCoreRoute.Delete(ACallbacks: array of THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Delete(FPath, ACallbacks);
-end;
-
-function THorseCoreRoute.Delete(AMiddleware, ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Delete(FPath, AMiddleware, ACallback);
-end;
-
-function THorseCoreRoute.Delete(ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Delete(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Delete(ACallbacks: array of THorseCallback; ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Delete(FPath, ACallbacks, ACallback);
-end;
-
-function THorseCoreRoute.Get(AMiddleware, ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Get(FPath, AMiddleware, ACallback);
-end;
-
-function THorseCoreRoute.Get(ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Get(FPath, ACallback, ACallback);
-end;
-
-function THorseCoreRoute.Get(ACallbacks: array of THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Get(FPath, ACallbacks);
-end;
-
-function THorseCoreRoute.Get(ACallbacks: array of THorseCallback; ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Get(FPath, ACallbacks, ACallback);
-end;
-
-function THorseCoreRoute.Head(ACallbacks: array of THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Head(FPath, ACallbacks);
-end;
-
-function THorseCoreRoute.Head(ACallbacks: array of THorseCallback; ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Head(FPath, ACallbacks, ACallback);
-end;
-
-function THorseCoreRoute.Head(AMiddleware, ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Head(FPath, AMiddleware, ACallback);
-end;
-
-function THorseCoreRoute.Head(ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Head(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Patch(ACallbacks: array of THorseCallback; ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Patch(FPath, ACallbacks, ACallback);
-end;
-
-function THorseCoreRoute.Patch(ACallbacks: array of THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Patch(FPath, ACallbacks);
-end;
-
-function THorseCoreRoute.Patch(AMiddleware, ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Patch(FPath, AMiddleware, ACallback);
-end;
-
-function THorseCoreRoute.Patch(ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Patch(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Post(ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Post(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Post(AMiddleware, ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Post(FPath, AMiddleware, ACallback);
-end;
-
-function THorseCoreRoute.Post(ACallbacks: array of THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Post(FPath, ACallbacks);
-end;
-
-function THorseCoreRoute.Post(ACallbacks: array of THorseCallback; ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Post(FPath, ACallbacks, ACallback);
-end;
-
-function THorseCoreRoute.Put(ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Put(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Put(AMiddleware, ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Put(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Put(ACallbacks: array of THorseCallback; ACallback: THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Put(FPath, ACallback);
-end;
-
-function THorseCoreRoute.Put(ACallbacks: array of THorseCallback): IHorseCoreRoute;
-begin
-  Result:= Self;
-  FHorseCore.Put(FPath, ACallbacks);
 end;
 
 end.
