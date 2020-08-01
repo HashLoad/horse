f725e993dd7b351850086306f9b623271adf9ef9
daniloacrz <daniloacr@gmail.com>
2020-08-01 09:17:07 -0300

    Update Horse.Constants.pas

--- src/Horse.Constants.pas
+++ src/Horse.Constants.pas
@@ -4,11 +4,12 @@ interface
 
 const
   DEFAULT_PORT = 9000;
+  DEFAULT_ADDR = '0.0.0.0';
   HORSE_ENV = 'HORSE_ENV';
   ENV_D = 'd';
   ENV_DEV = 'dev';
   ENV_DEVELOPMENT = 'development';
-  START_RUNNING = 'Server is runing on port %d';
+  START_RUNNING = 'Server is runing on %s:%d';
 
 implementation
 
