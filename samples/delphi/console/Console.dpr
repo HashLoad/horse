622b85917a0bcf6bf0753e1392d665ab5b860f14
CarlosHe <carlos@gabaritocorrigido.com.br>
2020-08-09 14:32:37 -0300

    Correction in the OnListen event and addition of the Console sample

---
+++ samples/delphi/console/Console.dpr
@@ -0,0 +1,24 @@
+program Console;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Horse, System.SysUtils;
+
+begin
+
+  THorse.Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+  THorse.Listen(9000,
+    procedure(Horse: THorse)
+    begin
+      Writeln(Format('Server is runing on %s:%d', [Horse.Host, Horse.Port]));
+    end);
+
+end.
