1a89f1445559f5d9b9de76eec098d44f4d62cdf8
CarlosHe <carlos@gabaritocorrigido.com.br>
2020-08-09 14:50:52 -0300

    Addition of the Daemon sample

---
+++ samples/delphi/daemon/Daemon.dpr
@@ -0,0 +1,20 @@
+program Daemon;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Horse, System.SysUtils;
+
+begin
+
+  THorse.Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+  THorse.Listen(9000);
+
+end.
