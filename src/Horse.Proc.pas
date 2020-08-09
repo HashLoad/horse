5b7151c33a8d21925e2a5861b1f6183b09f638a4
CarlosHe <carlos@gabaritocorrigido.com.br>
2020-08-08 22:47:53 -0300

    Lazarus execution problem solved

--- src/Horse.Proc.pas
+++ src/Horse.Proc.pas
@@ -10,11 +10,9 @@ interface
 
 type
 
+  TNextProc = procedure of object;
   TProc = procedure;
   TProc<T> = procedure (Arg1: T);
-  TProc<T1,T2> =  procedure (Arg1: T1; Arg2: T2);
-  TProc<T1,T2,T3> =  procedure (Arg1: T1; Arg2: T2; Arg3: T3);
-  TProc<T1,T2,T3,T4> = procedure (Arg1: T1; Arg2: T2; Arg3: T3; Arg4: T4);
 
 {$ENDIF}
 
