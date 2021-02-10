ac2afec9732ad4481717f37e57167a93a9a1419f
Pedro Braga <pedro@totall.com.br>
2021-02-09 21:12:35 -0300

    Ajuste para CompilerVersion do "Delphi 10.2 Tokyo / C++Builder 10.2 Tokyo"

--- src/Horse.HTTP.pas
+++ src/Horse.HTTP.pas
@@ -11,7 +11,7 @@ uses
   SysUtils, Classes, Generics.Collections, fpHTTP, HTTPDefs,
 {$ELSE}
   System.SysUtils, System.Classes, Web.HTTPApp, System.Generics.Collections,
-  {$IF CompilerVersion > 28.0}
+  {$IF CompilerVersion > 32.0}
   Web.ReqMulti,
   {$ENDIF}
 {$ENDIF}
