7cfeff43cacd8229e16c84c778359d92ca7d8fb7
Pedro Braga <pedro@totall.com.br>
2021-02-09 21:21:43 -0300

    Update Horse.HTTP.pas

--- src/Horse.HTTP.pas
+++ src/Horse.HTTP.pas
@@ -11,7 +11,7 @@ uses
   SysUtils, Classes, Generics.Collections, fpHTTP, HTTPDefs,
 {$ELSE}
   System.SysUtils, System.Classes, Web.HTTPApp, System.Generics.Collections,
-  {$IF CompilerVersion > 32.0}
+  {$IFDEF CompilerVersion > 32.0}
   Web.ReqMulti,
   {$ENDIF}
 {$ENDIF}
