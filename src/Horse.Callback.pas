eb995d470112fa7d1ee97a7c1007eddb37b0c958
Carlos Modesto <caiqi_04@hotmail.com>
2023-03-02 23:30:25 -0300

    mode switch function references for new fpc

--- src/Horse.Callback.pas
+++ src/Horse.Callback.pas
@@ -1,7 +1,10 @@
 unit Horse.Callback;
 
 {$IF DEFINED(FPC)}
-{$MODE DELPHI}{$H+}{$MODESWITCH FUNCTIONREFERENCES+}
+{$MODE DELPHI}{$H+}
+{$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}
+{$MODESWITCH FUNCTIONREFERENCES+}
+{$ENDIF}
 {$ENDIF}
 
 interface
@@ -20,13 +23,17 @@ uses
   Horse.Commons;
 
 type
+{$IF DEFINED(FPC)}
+  THorseCallbackRequest = {$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}reference to {$ENDIF}procedure(AReq: THorseRequest);
+  THorseCallbackResponse = {$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}reference to {$ENDIF}procedure(ARes: THorseResponse);
+  THorseCallbackRequestResponse = {$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}reference to {$ENDIF}procedure(AReq: THorseRequest; ARes: THorseResponse);
+  THorseCallback = {$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}reference to {$ENDIF}procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TNextProc);
+  TCallNextPath = function(var APath: TQueue<string>; const AHTTPType: TMethodType; const ARequest: THorseRequest; const AResponse: THorseResponse): Boolean of object;
+{$ELSE}
   THorseCallbackRequest = reference to procedure(AReq: THorseRequest);
   THorseCallbackResponse = reference to procedure(ARes: THorseResponse);
   THorseCallbackRequestResponse = reference to procedure(AReq: THorseRequest; ARes: THorseResponse);
   THorseCallback = reference to procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TNextProc);
-{$IF DEFINED(FPC)}
-  TCallNextPath = function(var APath: TQueue<string>; const AHTTPType: TMethodType; const ARequest: THorseRequest; const AResponse: THorseResponse): Boolean of object;
-{$ELSE}
   TCallNextPath = reference to function(var APath: TQueue<string>; const AHTTPType: TMethodType; const ARequest: THorseRequest; const AResponse: THorseResponse): Boolean;
 {$ENDIF}
 
