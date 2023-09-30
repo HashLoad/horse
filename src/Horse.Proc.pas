5d73880e6cf48c9cb7f2a72aa91d80a66fad2b97
Edson T. Marques <edsontmarques@users.noreply.github.com>
2023-09-29 21:05:51 -0300

    Sugestão de adequação da TProc na unit Horse.Proc para compatibilidade com funções anônimas

--- src/Horse.Proc.pas
+++ src/Horse.Proc.pas
@@ -2,6 +2,9 @@ unit Horse.Proc;
 
 {$IF DEFINED(FPC)}
   {$MODE DELPHI}{$H+}
+  {$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}
+    {$MODESWITCH FUNCTIONREFERENCES+}
+  {$ENDIF}
 {$ENDIF}
 
 interface
@@ -13,11 +16,11 @@ uses
 
 type
   TNextProc = {$IF DEFINED(FPC)}procedure of object{$ELSE}System.SysUtils.TProc{$ENDIF};
-  TProc = {$IF DEFINED(FPC)}procedure{$ELSE}System.SysUtils.TProc{$ENDIF};
+  TProc = {$IF DEFINED(FPC)}{$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}reference to {$ENDIF}procedure{$ELSE}System.SysUtils.TProc{$ENDIF};
 
-{$IF DEFINED(FPC)}
-  TProc<T> = procedure(Arg1: T);
-{$ENDIF}
+  {$IF DEFINED(FPC)}
+    TProc<T> = {$IF DEFINED(HORSE_FPC_FUNCTIONREFERENCES)}reference to {$ENDIF}procedure(Arg1: T);
+  {$ENDIF}
 
 implementation
 
