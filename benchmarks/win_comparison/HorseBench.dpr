eaaf5f8a66b9dabec6b250e7b728bb0a5b2b93f2
Régys Borges da Silveira <regys.silveira@gmail.com>
2026-06-29 13:45:40 -0300

    bench(comparison): add Windows/Linux comparison benchmarks between Horse and Dext

---
+++ benchmarks/win_comparison/HorseBench.dpr
@@ -0,0 +1,29 @@
+program HorseBench;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  Horse
+  {$IF DEFINED(HORSE_PROVIDER_IOCP)}
+  , Horse.Provider.IOCP
+  {$ELSEIF DEFINED(HORSE_PROVIDER_HTTPSYS)}
+  , Horse.Provider.HttpSys
+  {$ENDIF}
+  ;
+
+begin
+  try
+    THorse.Get('/ping',
+      procedure(Req: THorseRequest; Res: THorseResponse)
+      begin
+        Res.Send('pong');
+      end);
+
+    // Inicia o servidor na porta 9090
+    THorse.Listen(9090);
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
