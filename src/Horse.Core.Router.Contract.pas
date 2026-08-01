c692f4e8b99a0764bd9ece4c239ab4010368b186
viniciussanchez <viniciuss.sanchez@gmail.com>
2026-08-01 14:24:31 -0300

    Bug fixes: THorse.Routes.GetPrefix()

--- src/Horse.Core.Router.Contract.pas
+++ src/Horse.Core.Router.Contract.pas
@@ -20,6 +20,8 @@ uses
 type
   IHorseRouter = interface
     ['{69A48E4B-8E4B-4D4B-A69A-7D8E4B6D8E4B}']
+    function GetPrefix: string;
+    procedure Prefix(const APrefix: string);
     procedure RegisterRoute(const AHTTPType: TMethodType; const APath: string; const ACallback: THorseCallback);
     procedure RegisterRouteMiddleware(const AHTTPType: TMethodType; const APath: string; const ACallback: THorseCallback);
     procedure RegisterMiddleware(const APath: string; const AMiddleware: THorseCallback); overload;
